492eacb5f6739c5838ec20ee0680ee8b7c6ad33b
Tsafou <dinos1@hotmail.com>
2016-09-22 19:49:20 +0300

    fixed contact - fake send

--- components/contact/js/contactController.js
+++ components/contact/js/contactController.js
@@ -4,10 +4,28 @@
 
 angular.module('fleaMarket').controller('contactController', contactController);
 
-contactController.$inject = ['$scope', '$mdSidenav', '$state'];
+contactController.$inject = ['$scope', '$mdSidenav', '$state', '$timeout'];
 
-function contactController($scope, $mdSidenav, $state) {
+function contactController($scope, $mdSidenav, $state, $timeout) {
     var vm = this;
-    
+
+    vm.showSuccess = false;
+
+    $scope.person = {};
+
+    vm.sendEmail = function () {
+        if ($scope.contactForm.$valid) {
+            $timeout(function() {
+                $scope.person = {};
+                $scope.contactForm.$setPristine();
+                $scope.contactForm.$setUntouched();
+                vm.showSuccess = true;
+            }, 1500);
+            $timeout(function () {
+                vm.showSuccess = false;
+            }, 5500);
+        }
+
+    }
 
 }
